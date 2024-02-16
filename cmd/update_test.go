9cd9a446a28a47dd98f45082050f5ce8bd0f83cf
Owen Nelson <owen.nelson@thoughtworks.com>
2024-02-15 18:21:37 -0600

    Adhere to returning status from subcommands
    Authored-by: Owen Nelson <owen.nelson@thoughtworks.com>

--- cmd/update_test.go
+++ cmd/update_test.go
@@ -14,21 +14,21 @@ func TestIfUpdateAvailable(t *testing.T) {
 	um := UpdateManager{updater: selfupdate.DefaultUpdater(), repository: selfupdate.ParseSlug("thoughtworks/talisman"), output: &output}
 	oldVersion := "v1.20.0"
 	currentRelease, _, _ := selfupdate.DetectLatest(context.Background(), selfupdate.ParseSlug("thoughtworks/talisman"))
-	um.CanUpdateFrom(context.Background(), oldVersion)
+	um.Check(context.Background(), oldVersion)
 	assert.True(t, currentRelease.GreaterThan(oldVersion), "There is an update available for this old version.")
 	assert.Equal(t, UpdateMessage(currentRelease.Version()), output.String(), "There is an update available for this old version.")
 }
 func TestNoUpdateAvailableForInvalidRepository(t *testing.T) {
 	var output bytes.Buffer
 	invalidUm := UpdateManager{updater: selfupdate.DefaultUpdater(), repository: selfupdate.ParseSlug("/bad-repo"), output: &output}
-	invalidUm.CanUpdateFrom(context.Background(), "v1.32.0")
+	invalidUm.Check(context.Background(), "v1.32.0")
 	assert.True(t, output.String() == "", "We should not suggest updating if there might not be an update. This simulates network errors or GitHub rate limiting.")
 }
 
 func TestNoUpdateAvailableIfNoReleaseFound(t *testing.T) {
 	var output bytes.Buffer
 	invalidUm := UpdateManager{updater: selfupdate.DefaultUpdater(), repository: selfupdate.ParseSlug("thoughtworks/thoughtworks.github.io"), output: &output}
-	invalidUm.CanUpdateFrom(context.Background(), "v0.0.0")
+	invalidUm.Check(context.Background(), "v0.0.0")
 	assert.True(t, output.String() == "", "There is no update available if there are no releases")
 }
 
@@ -36,14 +36,14 @@ func TestNoUpdateAvailableIfOnCurrentVersion(t *testing.T) {
 	currentRelease, _, _ := selfupdate.DetectLatest(context.Background(), selfupdate.ParseSlug("thoughtworks/talisman"))
 	var output bytes.Buffer
 	um := UpdateManager{updater: selfupdate.DefaultUpdater(), repository: selfupdate.ParseSlug("thoughtworks/talisman"), output: &output}
-	um.CanUpdateFrom(context.Background(), currentRelease.Version())
+	um.Check(context.Background(), currentRelease.Version())
 	assert.True(t, output.String() == "", "There is no update available if on the current version")
 }
 
 func TestNoUpdateIfUnexpectedCurrentVersion(t *testing.T) {
 	var output bytes.Buffer
 	um := UpdateManager{updater: selfupdate.DefaultUpdater(), repository: selfupdate.ParseSlug("thoughtworks/talisman"), output: &output}
-	um.CanUpdateFrom(context.Background(), "Local dev version")
+	um.Check(context.Background(), "Local dev version")
 	assert.True(t, output.String() == "", "There is no update available if not on a published version")
 
 }
